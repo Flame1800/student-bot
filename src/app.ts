74975286ec6fa0f5375a032082e8f741970626aa
flame1800 <r.shaficoff@gmail.com>
2024-01-12 17:42:23 +0500

    rm express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 import Bot from "./bot";
 import { ConfigService } from "./config/config.service";
-import express from "express";
 import { MongoClient } from "mongodb";
-
-const app = express();
 
 mongoose.connect("mongodb://localhost:27017/schedule")
 
@@ -12,9 +9,4 @@ MongoClient.connect("mongodb://localhost:27017/schedule").then((client) => {
   const db = client.db();
   const bot = new Bot(db, new ConfigService());
   bot.init();
-
-  const port = 6001;
-  app.listen(port);
-
-  console.log(`Server is start on port ${port}`);
 });
